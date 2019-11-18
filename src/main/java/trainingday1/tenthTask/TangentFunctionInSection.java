a26c7b90e77b24e5ced817288334a99c9094c242
Sergio1234567z <raboch2014@mail.ru>
2019-11-18 21:43:20 +0300

    Changed test in tenthtask

--- src/main/java/trainingday1/tenthTask/TangentFunctionInSection.java
+++ src/main/java/trainingday1/tenthTask/TangentFunctionInSection.java
@@ -1,25 +1,34 @@
 package trainingday1.tenthTask;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TangentFunctionInSection {
     DecimalFormat df;
+    List toReturn;
 
     TangentFunctionInSection() {
         df = new DecimalFormat("#0.00");
     }
 
-    public void calculationFunction(double firstPoint, double secondPoint, double step) {
+    public List calculationFunction(double firstPoint, double secondPoint, double step) {
+        toReturn = new ArrayList<String>();
         double point = firstPoint;
         while (point <= secondPoint) {
-            double resalt = Math.sin(point) / Math.cos(point);
-            System.out.println("x = " + point + "    F(x) = " + df.format(resalt));
+            double resalt = Math.tan(point);
+            String toArray = "x=" + point + "F(x)=" + df.format(resalt);
+            toReturn.add(toArray);
             point += step;
         }
+        return toReturn;
     }
 
     public static void main(String[] args) {
         TangentFunctionInSection forTest = new TangentFunctionInSection();
-        forTest.calculationFunction(1, 5, 1);
+        List test = forTest.calculationFunction(0.1, 3, 0.5);
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println(test.get(i));
+        }
     }
 }
--- src/test/java/trainingday1/tenthTask/TangentFunctionInSectionTest.java
+++ src/test/java/trainingday1/tenthTask/TangentFunctionInSectionTest.java
@@ -1,30 +1,53 @@
 package trainingday1.tenthTask;
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TangentFunctionInSectionTest {
-//    String[] firstPointResalt;
-//    String[] secondPointResalt;
-//
-//    TangentFunctionInSection forTest = new TangentFunctionInSection();
-//
-//    @BeforeClass
-//    public void initialization() {
-//        String y = "1, 5, 1";
-//        String t = "forTest.calculationFunction(0.5, 3, 0.5)";
-//        String[] firstPointResalt = {"x = 1.0    F(x) = 1,56", "x = 2.0    F(x) = -2,19", "x = 3.0    F(x) = -0,14", "x = 4.0    F(x) = 1,16",
-//                "x = 5.0    F(x) = -3,38"};
-//        String[] secondPointResalt = {"x = 0.5    F(x) = 0,55", "x = 1.0    F(x) = 1,56", "x = 1.5    F(x) = 14,10", "x = 2.0    F(x) = -2,19",
-//                "x = 2.5    F(x) = -0,75","x = 3.0    F(x) = -0,14"};
-//    }
-//
-//    @Test
-//    public void checkFunction(){
-//        for (int i = 0;i<firstPointResalt.length;i++){
-//            Assert.assertEquals(firstPointResalt[i],);
-//        }
+
+
+    TangentFunctionInSection forTest = new TangentFunctionInSection();
+    List firstPointResalt = new ArrayList();
+    List secondPointResalt = new ArrayList();
+    List thirdPoint = new ArrayList();
+
+    @Before
+    public void initialization() {
+        firstPointResalt.add("x=1.0F(x)=1,56");
+        firstPointResalt.add("x=2.0F(x)=-2,19");
+        firstPointResalt.add("x=3.0F(x)=-0,14");
+        firstPointResalt.add("x=4.0F(x)=1,16");
+        firstPointResalt.add("x=5.0F(x)=-3,38");
+
+        secondPointResalt.add("x=0.5F(x)=0,55");
+        secondPointResalt.add("x=1.0F(x)=1,56");
+        secondPointResalt.add("x=1.5F(x)=14,10");
+        secondPointResalt.add("x=2.0F(x)=-2,19");
+        secondPointResalt.add("x=2.5F(x)=-0,75");
+        secondPointResalt.add("x=3.0F(x)=-0,14");
+        secondPointResalt.add("x=3.5F(x)=0,37");
+        secondPointResalt.add("x=4.0F(x)=1,16");
+        secondPointResalt.add("x=4.5F(x)=4,64");
+        secondPointResalt.add("x=5.0F(x)=-3,38");
+
+        thirdPoint.add("x=0.1F(x)=0,10");
+        thirdPoint.add("x=0.6F(x)=0,68");
+        thirdPoint.add("x=1.1F(x)=1,96");
+        thirdPoint.add("x=1.6F(x)=-34,23");
+        thirdPoint.add("x=2.1F(x)=-1,71");
+        thirdPoint.add("x=2.6F(x)=-0,60");
     }
+
+    @Test
+    public void checkFunction() {
+        Assert.assertEquals(firstPointResalt, forTest.calculationFunction(1, 5, 1));
+        Assert.assertEquals(secondPointResalt, forTest.calculationFunction(0.5, 5, 0.5));
+        Assert.assertEquals(firstPointResalt, forTest.calculationFunction(1, 5, 1));
+    }
+}
 
 
